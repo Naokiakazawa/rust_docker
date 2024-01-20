d0efe3fab3146c2d807addf127ebffde157be5f6
Naokiakazawa <naoki9akazawa@gmail.com>
2024-01-20 17:18:46 +0900

    setup http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    log::info!("Starting server");
+
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(hello))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
